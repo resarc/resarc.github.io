c0a22c76b38be4f4e31e2600be4aa8d540d84cf0
resarc <35898472+resarc@users.noreply.github.com>
2024-08-05 21:30:33 +0700

    Add files via upload

---
+++ assets/menu-D07ve1ke.js
@@ -0,0 +1,7 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function c(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=c(e);fetch(e.href,r)}})();let n=document.createElement("div");n.classList.add("menu-container");n.innerHTML=`
+<a href="/projects">Projects</a>
+<a href="/week-01">Week 01</a>
+<a href="/week-02">Week 02</a>
+<a href="/week-03">Week 03</a>
+<a href="/week-04">Week 04</a>
+`;document.querySelector(".menu").appendChild(n);
---
+++ assets/week04-DNsLCIaf.js
@@ -0,0 +1,30 @@
+import"./menu-D07ve1ke.js";const t=`<h1>JavaScript</h1>
+<p>Comment</p>
+<pre><code class="language-js">// a comment
+/* also a comment */
+</code></pre>
+<hr>
+<h2>Variables</h2>
+<pre><code class="language-js">let age = 23; // Number
+let name = &quot;John&quot;; // String
+let opened = true; // Boolean
+</code></pre>
+<p>info: <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let">https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let</a></p>
+<hr>
+<h2>Selectors</h2>
+<h3>querySelector()</h3>
+<p>Select the first element that matches the query</p>
+<pre><code class="language-js">let li = document.querySelector(&quot;li&quot;);
+let day = document.querySelector(&quot;.day&quot;);
+let paragraph = document.querySelector(&quot;ul#list p&quot;);
+</code></pre>
+<p>info: <a href="https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector">https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector</a></p>
+<hr>
+<h2>Element manipulation</h2>
+<h3>add classes</h3>
+<pre><code class="language-js">jslet divElement = document.querySelector(&quot;div&quot;);
+divElement.classList.add(&quot;small&quot;);
+divElement.classList.remove(&quot;big&quot;);
+</code></pre>
+<p>info: <a href="https://developer.mozilla.org/en-US/docs/Web/API/Element/classList">https://developer.mozilla.org/en-US/docs/Web/API/Element/classList</a></p>
+`;let e=document.createElement("div");e.classList.add("content");e.setAttribute("id","week-04");e.innerHTML=t;document.querySelector("#section-week-04").appendChild(e);
